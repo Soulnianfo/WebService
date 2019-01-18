f544c5af47a1348ee184f3bde9760b9643100ec6
Adel Touaibia <adel.touaibia23@gmail.com>
2019-01-18 12:52:35 +0100

    Changed Security web config

--- src/main/java/com/classphoto2/classphoto2/SecurityConfig.java
+++ src/main/java/com/classphoto2/classphoto2/SecurityConfig.java
@@ -6,6 +6,13 @@
 package com.classphoto2.classphoto2;
 
 import com.classphoto2.classphoto2.service.MyUserDetailService;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,8 +20,12 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -30,6 +41,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private LoggingAccessDeniedHandler accessDeniedHandler;
 
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
@@ -51,7 +64,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                     .loginPage("/login")
-                    .defaultSuccessUrl("/school/accueilSchool").passwordParameter("password").usernameParameter("username")
+                    .passwordParameter("password").usernameParameter("username").successHandler(new AuthenticationSuccessHandler() {
+	                    @Override
+	                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+	                            Authentication authentication) throws IOException, ServletException {
+	                        redirectStrategy.sendRedirect(request, response, "/school/accueilSchool");
+	                    }
+	                })
                     //.failureUrl("/access-denied")
                     .permitAll()
                 .and()
@@ -65,7 +84,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling()
                     .accessDeniedHandler(accessDeniedHandler);
         //http.addFilterAfter(new CsrfTokenResponseHeaderBindingFilter(), CsrfFilter.class);
-                
+                //"/school/accueilSchool"
     }
 
     @Override
